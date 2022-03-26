99f1ea5ebe54907b3e649a6a93ef2a94a59e02a7
Kevin Souza <igantekevin@hotmail.com>
2022-03-26 18:07:27 -0300

    install breeeze

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,5 @@
 <?php
 
-use App\Http\Controllers\{
-    ClassRoomController,
-    ExamController
-};
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,28 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::resource('classrooms', ClassRoomController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::prefix('/classrooms')->group(function(){
-    Route::post('/', [ClassRoomController::class,'store'])->name('classrooms.store')->can('create','App\Models\Classroom');
-    Route::get('/', [ClassRoomController::class,'index'])->name('classrooms.index');
-    Route::get('/{classroom}', [ClassRoomController::class,'show'])->name('classrooms.show');
-    Route::delete('/{classroom}', [ClassRoomController::class,'delete'])->name('classrooms.delete');
-    
-    Route::post('/{classroom}/assingstudent', [ClassRoomController::class, 'assingStudent'])
-    ->name('classrooms.assingstudent')->can('update', 'classroom');
-
-    Route::delete('/{classroom}/assingstudent', [ClassRoomController::class, 'unassingStudent'])
-    ->name('classrooms.unassingstudent')->can('update', 'classroom');
-});
-
-
-
-
-
-//Route::resource('exams', ExamController::class);
-
-Route::prefix('/exams')->group(function(){
-    Route::post('/',[ExamController::class,'store'])->name('exams.store')->can('create','App\Models\Exam');
-    Route::get('/{exam}',[ExamController::class,'show'])->name('exams.show')->can('viewExam','exam');
-});
+require __DIR__.'/auth.php';
